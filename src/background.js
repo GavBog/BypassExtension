b12d7d9b9a880f0e5a2c141006fff388949d7870
Gavin Bogie <me@gbogie.com>
2023-05-28 20:33:02 -0400

    Change URL, Format, and Rework Regex.

--- src/background.js
+++ src/background.js
@@ -1,33 +1,45 @@
 // Activates when extension is first installed
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.runtime.openOptionsPage();
+  chrome.runtime.openOptionsPage();
 });
 
 var url = "";
 // Activates when Omnibox is used
 chrome.omnibox.onInputEntered.addListener(function (text) {
-    // Test if url is valid
-    if (/^https?:\/\/?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/.test(text)) {
-        url = text;
-    } else if (/^[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/.test(text)) {
-        url = "http://" + text;
-    } else {
-        url = "https://www.google.com/search?q=" + text;
-    }
+  // Test if URL is valid
+  if (
+    /^(?:https?:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)$/.test(
+      text
+    )
+  ) {
+    url =
+      text.startsWith("http://") || text.startsWith("https://")
+        ? text
+        : "http://" + text;
+  } else {
+    url = "https://www.google.com/search?q=" + text;
+  }
 
-    // Encoding Settings
-    chrome.storage.sync.get({
-        encoding: 'default'
-    }, function (items) {
-        switch (items.encoding) {
-            // Open requested URL
-            case 'default':
-                chrome.tabs.create({ url: "https://browse.projectocasta.org/main/" + url });
-                break;
-            case 'b64':
-                url = btoa(url);
-                chrome.tabs.create({ url: "https://dev.browse.projectocasta.org/b64/" + url });
-                break;
-        }
-    });
+  // Encoding Settings
+  chrome.storage.sync.get(
+    {
+      encoding: "default",
+    },
+    function (items) {
+      switch (items.encoding) {
+        // Open requested URL
+        case "default":
+          chrome.tabs.create({
+            url: "https://browse.projectocasta.org/main/" + url,
+          });
+          break;
+        case "b64":
+          url = btoa(url);
+          chrome.tabs.create({
+            url: "https://browse.projectocasta.org/b64/" + url,
+          });
+          break;
+      }
+    }
+  );
 });
--- src/options.js
+++ src/options.js
@@ -1,21 +1,27 @@
 function save_options() {
-    var encoding = document.getElementById('encoding').value;
-    chrome.storage.sync.set({
-        encoding: encoding
-    }, function () {
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 750);
-    });
+  var encoding = document.getElementById("encoding").value;
+  chrome.storage.sync.set(
+    {
+      encoding: encoding,
+    },
+    function () {
+      var status = document.getElementById("status");
+      status.textContent = "Options saved.";
+      setTimeout(function () {
+        status.textContent = "";
+      }, 750);
+    }
+  );
 }
 function restore_options() {
-    chrome.storage.sync.get({
-        encoding: 'default'
-    }, function (items) {
-        document.getElementById('encoding').value = items.encoding;
-    });
+  chrome.storage.sync.get(
+    {
+      encoding: "default",
+    },
+    function (items) {
+      document.getElementById("encoding").value = items.encoding;
+    }
+  );
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.addEventListener("DOMContentLoaded", restore_options);
+document.getElementById("save").addEventListener("click", save_options);
